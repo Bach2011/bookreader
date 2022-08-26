2115466f811e83caaeb5e261f6c82a0ea3cd0ea9
Nguyen Tuong Bach <87748138+Bach2011@users.noreply.github.com>
2022-08-26 21:22:40 +0700

    Ahih

--- reader/views.py
+++ reader/views.py
@@ -7,16 +7,17 @@ import PyPDF2
 def index(request):
     if request.method == 'POST':
         path = request.POST.get('location')
-        lang = request.POST.get('lang')
         pdfReader = PyPDF2.PdfReader(path)
         numpages = pdfReader.numPages
         text = ""
         for i in range(1,numpages+1):
             pageobj = pdfReader.getPage(i-1)
             text += pageobj.extract_text()
+        text.replace(';','\n')
+        read_text = text.replace("'","").replace('"',"").replace('Im',"I am")
         return render(request, "reader/reader.html", {
             "text":text,
-            "lang":lang
+            "read_text":read_text
         })
     else:
         return render(request, "reader/index.html")
